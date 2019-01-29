2c45dc16331658594735d9dd110f389483faadb3
LANIT\KNosov <knosov@lanit.ru>
2019-01-29 12:02:31 +0300

    CreateRepo

---
+++ FigureCalc/Figures/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace FigureCalc
+{
+    class Circle : AbstractFigure
+    {
+        private double _radius;
+
+        public Circle(double radius)
+        {
+            if (radius < 0)
+            {
+                throw new ArgumentException("The Radius can't be negative");
+            }
+
+            _radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * _radius * _radius;
+        }
+    }
+}
